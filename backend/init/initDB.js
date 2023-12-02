ad12f31af57c3a44094b4fed7b68f1de0058557e
Eclipsezty <ZHONG17@student.unimelb.edu.au>
2023-12-02 22:54:36 +1100

    finish initialization of DB

--- backend/init/initDB.js
+++ backend/init/initDB.js
@@ -1,9 +1,25 @@
 const dbconnection = require('../db/connection')
+const sequelize = require('../db/sequelize')
+const User = require('../db/model_user')
+const Record = require('../db/model_user_record')
+
+
+const initRelation = ()=>{
+    User.hasMany(Record,{
+        //if user is deleted, the record will be deleted as well
+        onDelete:'CASCADE'
+    })
+    Record.belongsTo(User)
+}
 
 const initDB = ()=>{
     return new Promise(async (resolve, reject) => {
         try {
             await dbconnection()
+
+            initRelation()
+            //sync all models and relations
+            await sequelize.sync({alter:true})
             resolve()
         } catch (error) {
             console.log(error)
